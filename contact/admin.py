4bd6739d1049d504412276c1632c9cecc37dc07d
VeyselKaraali <veysel-karaali@outlook.de>
2024-06-12 15:22:22 +0200

    Registered Models in admin.py

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from contact.models import Contact
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    search_fields = ['id', 'name']
+
 
 # Register your models here.
+admin.site.register(Contact, ContactAdmin)
--- subtask/admin.py
+++ subtask/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from subtask.models import Subtask
+
+class SubtaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'get_task_id', 'is_done', 'description']
+    search_fields = ['id', 'task__id', 'description']
+    list_filter = ['is_done']
+
+    def get_task_id(self, obj):
+        return obj.task.id
+    get_task_id.short_description = 'Task ID'
+
+
 # Register your models here.
+admin.site.register(Subtask, SubtaskAdmin)
