c8b40a4bc2fe86df2d697a14e6459ef80dc8ac7a
VeyselKaraali <veysel-karaali@outlook.de>
2024-06-12 15:38:32 +0200

    Configuration and customization of the Django admin interface

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-
 from user.models import User
 
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'email']
+    search_fields = ['id', 'name', 'email']
+
+
 # Register your models here.
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
