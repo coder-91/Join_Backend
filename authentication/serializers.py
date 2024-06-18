4d6de6d507539aeca28603711e7c85b3368ab1ee
VeyselKaraali <veysel-karaali@outlook.de>
2024-06-18 15:56:24 +0200

    WIP: Implementing authentication rest api

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,8 +1,33 @@
 from rest_framework import serializers
+
+from authentication import models
 from authentication.models import UserProfile
 
 
-class UserProfileSerializer(serializers.HyperlinkedModelSerializer):
+class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
         fields = '__all__'
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {'input_type': 'password'}
+            }
+        }
+
+    def create(self, validated_data):
+        """Create and return a new user"""
+        user = models.UserProfile.objects.create_user(
+            email=validated_data['email'],
+            name=validated_data['name'],
+            password=validated_data['password']
+        )
+        return user
+
+    def update(self, instance, validated_data):
+        """Handle updating user account"""
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
+            instance.set_password(password)
+
+        return super().update(instance, validated_data)
---
+++ authentication/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from authentication import views
+
+urlpatterns = [
+    path('register/', views.RegisterView.as_view()),
+    path('login/', views.LoginView.as_view()),
+    path('logout/', views.LogoutView.as_view()),
+]
