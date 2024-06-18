8d643b74a51c79a144f2ebdc24bee20ddb90909c
VeyselKaraali <veysel-karaali@outlook.de>
2024-06-18 02:33:38 +0200

    Implemented rest API for contact

--- contact/serializers.py
+++ contact/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from contact.models import Contact
 
 
-class ContactSerializer(serializers.HyperlinkedModelSerializer):
+class ContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contact
         fields = ['id', 'email', 'name', 'phone_number', 'avatar_color']
--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.authentication import TokenAuthentication
+
+
 from contact.models import Contact
 from contact.serializers import ContactSerializer
 
@@ -7,4 +10,5 @@ from contact.serializers import ContactSerializer
 class ContactViewSet(viewsets.ModelViewSet):
     queryset = Contact.objects.all().order_by('id')
     serializer_class = ContactSerializer
+    #authentication_classes = (TokenAuthentication,)
     permission_classes = []
