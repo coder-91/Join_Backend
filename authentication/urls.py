9a9024d68ebd1247a7a72cd9820669c29cd91a85
VeyselKaraali <veysel-karaali@outlook.de>
2024-06-20 17:31:44 +0200

    Deleted apps authentication and contacts

--- authentication/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from authentication import views
-
-urlpatterns = [
-    path('register/', views.RegisterView.as_view()),
-    path('login/', views.LoginView.as_view()),
-    path('logout/', views.LogoutView.as_view()),
-]
