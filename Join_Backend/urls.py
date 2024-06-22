0fdcfeb3800509835063ee8aadbba0827a34f1fd
VeyselKaraali <veysel-karaali@outlook.de>
2024-06-22 06:40:09 +0200

    Updated urls

--- Join_Backend/urls.py
+++ Join_Backend/urls.py
@@ -1,22 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-from subtask.views import SubtaskViewSet
-from task.views import TaskViewSet
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
 
-router = routers.DefaultRouter()
-router.register(r'tasks', TaskViewSet, basename='tasks')
-router.register(r'subtasks', SubtaskViewSet, basename='subtasks')
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    path('', include('user.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'), name='api-docs',)
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'), name='api-docs'),
+    path('users/', include('user.urls')),
+    path('tasks/', include('task.urls')),
+    path('subtasks/', include('subtask.urls')),
 ]
