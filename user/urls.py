e120161817860eb63df87ac7a621933c14eae9be
VeyselKaraali <veysel-karaali@outlook.de>
2024-06-24 16:09:25 +0200

    Added url to list all users

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,12 @@
 """URL mappings for the user API."""
 
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 from user import views
 
+router = DefaultRouter()
+router.register('', views.UserViewSet)
 
 app_name = 'user'
 
@@ -11,4 +14,5 @@ urlpatterns = [
     path('register/', views.CreateUserView.as_view(), name='register'),
     path('token/', views.CreateTokenView.as_view(), name='token'),
     path('me/', views.ManageUserView.as_view(), name='me'),
+    path('', include(router.urls)),
 ]
