35baf41b8eb79eddfd2567e63e934dccfa0fc509
VeyselKaraali <veysel-karaali@outlook.de>
2024-06-27 16:04:00 +0200

    Updated task serializer

--- task/serializers.py
+++ task/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
+
+from subtask.serializers import SubtaskSerializer
 from task.models import Task
+from user.serializers import UserSerializer
 
 
 class TaskSerializer(serializers.ModelSerializer):
     """Serializes a task object"""
+    subtasks = SubtaskSerializer(many=True)
+    users = UserSerializer(many=True)
 
     class Meta:
         model = Task
