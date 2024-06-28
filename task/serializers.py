4acad48befb3398c81390baa8a5d4820c9572a79
VeyselKaraali <veysel-karaali@outlook.de>
2024-06-28 12:35:32 +0200

    Updated task serializer

--- task/serializers.py
+++ task/serializers.py
@@ -2,16 +2,28 @@ from rest_framework import serializers
 
 from subtask.serializers import SubtaskSerializer
 from task.models import Task
+from user.models import User
 from user.serializers import UserSerializer
 
 
 class TaskSerializer(serializers.ModelSerializer):
     """Serializes a task object"""
-    subtasks = SubtaskSerializer(many=True)
-    users = UserSerializer(many=True)
+    subtasks = SubtaskSerializer(many=True, required=False)
+    users = UserSerializer(many=True, required=False)
 
     class Meta:
         model = Task
         fields = ['id', 'title', 'description', 'due_to', 'created', 'updated', 'priority', 'category', 'status',
                   'subtasks', 'users']
-        read_only_fields = ['created', 'updated', 'users']
+        read_only_fields = ['created', 'updated']
+
+    def create(self, validated_data):
+        users_data = validated_data.pop('users')
+        task = Task.objects.create(**validated_data)
+
+        if users_data is not None:
+            for user_data in users_data:
+                user = User.objects.get(id=user_data['id'])
+                task.users.add(user)
+
+        return task
