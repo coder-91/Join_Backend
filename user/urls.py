1e1952e711226ec622ca76845cdcd61143ce7658
VeyselKaraali <veysel-karaali@outlook.de>
2024-07-04 16:04:58 +0200

    Implemented Logout

--- user/urls.py
+++ user/urls.py
@@ -12,6 +12,7 @@ app_name = 'user'
 
 urlpatterns = [
     path('register/', views.CreateUserView.as_view(), name='register'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
     path('token/', views.CreateTokenView.as_view(), name='token'),
     path('me/', views.ManageUserView.as_view(), name='me'),
     path('', include(router.urls)),
--- user/views.py
+++ user/views.py
@@ -1,12 +1,14 @@
 """Views for the user API."""
-
+from django.contrib.auth import logout
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.settings import api_settings
-from rest_framework import filters
+from rest_framework import filters, status
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.views import APIView
 
 from user import permissions
 from user.models import User
@@ -43,6 +45,16 @@ class CreateTokenView(ObtainAuthToken):
 class UserLoginApiView(ObtainAuthToken):
     """Handle creating user authentication tokens"""
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class LogoutView(APIView):
+    """Logout the authenticated user."""
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        logout(request)
+        return Response(status=status.HTTP_200_OK)
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
